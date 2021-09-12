308cb9ea8d8df00f2c39db62faeeb9bde5f876ae
7Aylon <athlarster@gmail.com>
2021-09-12 17:38:15 +0200

    info icons

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ import '../styles/page.scss'
 function Contact({ pageEngaged, setPageEngaged }) {
 
     const [mousePos, setMousePos] = useState({ x: 0, y: 0 })
-    const [skills, setSkills] = useState(false)
+    const [infoShown, setInfoShown] = useState(false)
 
     let image = []
 
@@ -30,7 +30,7 @@ function Contact({ pageEngaged, setPageEngaged }) {
             onMouseEnter={() => setPageEngaged(true)}
             // onMouseLeave={() => setPageEngaged(false)}
             onMouseMove={(e) => mouse(e)}
-            className={pageEngaged && skills ? "page-background  contact magnified-contact" : pageEngaged ? "page-background contact " : "page-background"}>
+            className={pageEngaged && infoShown ? "page-background  contact magnified-contact" : pageEngaged ? "page-background contact " : "page-background"}>
             {pageEngaged && <div style={{ top: mousePos.y + 2, left: mousePos.x + 2 }} className="mouse"></div>}
             <div className="img-container ">
                 {image.map(row => row.map(img => {
@@ -46,10 +46,14 @@ function Contact({ pageEngaged, setPageEngaged }) {
                         className="img-part"></div>
                 }))}
             </div>
-            <div onMouseEnter={() => setSkills(true)} onMouseLeave={() => setSkills(false)} className="contact-info-container">lalala</div>
-
-
-            {!skills && <div onClick={() => setPageEngaged(false)} className={pageEngaged ? "main-text-hidden" : "main-text"}>CONTACT</div>}
+            <div onMouseEnter={() => setInfoShown(true)} className={infoShown ? "info-icon-hidden" : "contact-icon"}></div>
+            <div className="contact-info-container">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            {!infoShown && <div onClick={() => setPageEngaged(false)} className={pageEngaged ? "main-text-hidden" : "main-text"}>CONTACT</div>}
 
         </div >
     );
