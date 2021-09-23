6d7336c84c410b0836c2bdc975975874f1b86c46
7Aylon <athlarster@gmail.com>
2021-09-23 23:22:48 +0200

    about me text

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -24,7 +24,14 @@ function AboutMe({ pageEngaged, setPageEngaged }) {
             onMouseMove={(e) => mouse(e)}
             className={pageEngaged && info ? " page-background about-me magnified-2 " : pageEngaged ? "page-background about-me" : "page-background"}>
             <Background pageEngaged={pageEngaged} picture={picture} />
-            <div onMouseEnter={() => setInfo(true)} onMouseLeave={() => setInfo(false)} className={pageEngaged ? "me-text" : "me-text-hidden"}> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui, facilis?</div>
+            <div onMouseEnter={() => setInfo(true)} onMouseLeave={() => setInfo(false)} className={pageEngaged ? "me-text" : "me-text-hidden"}>
+
+                Engulfed by the discovery of my latent passion to computer since in 2020 I started to immerse myself in this bizzare world of ones and zeros.
+                Not only did logic gates kindled my curiosity but also they made me aware of the numerous levels of abstracion behind the scenes.
+                Intermingling of a scientific and humanistic mind pushed me towards both front-end and back-end developement. Being a young apprentice
+                of the coding order I know that I still "must unlearn what I have learned".
+
+            </div>
             {pageEngaged && <div className="aboutme-info-container">
                 <div className='about-me-icon'>Lorem ipsum dolor sit amet consectetur adipisicing elit. At esse dolores minima reprehenderit officia ad.</div>
                 <div className='about-me-icon-2'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id et labore explicabo doloremque tenetur tempora eos architecto consequuntur cum nam.</div>
