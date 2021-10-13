7dc9dd27ef6ba8b4604001f728f7f291d87af47e
7Aylon <athlarster@gmail.com>
2021-10-13 08:24:15 +0200

    my projects position absolute css

--- src/components/MyProjects.jsx
+++ src/components/MyProjects.jsx
@@ -8,22 +8,24 @@ import ProjectLink from './ProjectLink';
 function MyProjects({ pageEngaged, setPageEngaged }) {
 
     const [skills, setSkills] = useState(false)
+    console.log(skills)
 
     return (
         <div
-            // onMouseLeave={() => setPageEngaged(false)}
+
             className={pageEngaged && skills ? " page-background my-projects magnified" : pageEngaged ? "page-background my-projects " : "page-background"}>
 
             <Background pageEngaged={pageEngaged} picture={picture} />
-            {pageEngaged && <div onMouseEnter={() => setSkills(true)} onMouseLeave={() => setSkills(false)} className="projects-container">
-                <ProjectLink liveLink="https://englishempowering.com/" gitLink="https://englishempowering.com/"
+            {pageEngaged && <div onClick={() => setSkills(true)} className={skills ? "projects-container-open" : "projects-container"}>my projects
+                <ProjectLink skills={skills} liveLink="https://englishempowering.com/" gitLink="https://englishempowering.com/"
                     description="Website / App - my online business card where students can book English lessons." />
-                <ProjectLink liveLink="https://englishempowering.com/" gitLink="https://englishempowering.com/"
+                <ProjectLink skills={skills} liveLink="https://englishempowering.com/" gitLink="https://englishempowering.com/"
                     description="Tile Wars - a board game" />
-                <ProjectLink liveLink="https://englishempowering.com/" gitLink="https://englishempowering.com/"
+                <ProjectLink skills={skills} liveLink="https://englishempowering.com/" gitLink="https://englishempowering.com/"
                     description="- Sorting Alogrithm" />
-                <ProjectLink liveLink="https://englishempowering.com/" gitLink="https://englishempowering.com/"
+                <ProjectLink skills={skills} liveLink="https://englishempowering.com/" gitLink="https://englishempowering.com/"
                     description="Calculator - a simple project using vanilla JS" />
+                <div onMouseEnter={() => setSkills(false)} className={skills ? "project-close-icon" : "project-close-icon-hidden"} />
             </div>}
             <div onClick={() => setPageEngaged(!pageEngaged)} className={pageEngaged ? "main-text-hidden" : "main-text"}>{pageEngaged ? "MENU" : "MY PROJECTS"}</div>
         </div>
--- src/components/ProjectLink.jsx
+++ src/components/ProjectLink.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import '../styles/my-projects.scss'
 
-function ProjectLink({ description, gitLink, liveLink }) {
+function ProjectLink({ skills, description, gitLink, liveLink }) {
     return (
         <div>
-            <div className="projects-item">{description}</div>
-            <div className="link-container">
-                <a className="link" href={gitLink}>GitHub</a>
-                <a className="link trans-2" href={liveLink}>Live</a>
+            <div className={skills ? "projects-item-visible" : "projects-item"}>{description}</div>
+            <div className={skills ? "link-container-visible" : "link-container"}>
+                <a className={skills ? "link-visible" : "link"} href={gitLink}>GitHub</a>
+                <a className={skills ? "link-visible-2" : "link-2"} href={liveLink}>Live</a>
             </div>
         </div>
     );
