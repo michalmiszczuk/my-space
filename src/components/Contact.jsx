0e51583991b09ade9140494042f8957233dd5005
7Aylon <athlarster@gmail.com>
2021-10-19 21:40:43 +0200

    contact media queries small added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,9 +11,12 @@ function Contact({ pageChanged, pageEngaged, setPageEngaged }) {
 
     return (
         <div
-            className={pageEngaged && infoShown ? "page-background contact " : pageEngaged ? "page-background contact grey-contact" : "page-background"}>
+            className={pageEngaged && infoShown ? "page-background contact moveup" : pageEngaged ? "page-background contact grey-contact" : "page-background"}>
             <Background pageChanged={pageChanged} pageEngaged={pageEngaged} picture={picture} />
-            {pageEngaged && <div onClick={() => setInfoShown(false)} onMouseEnter={() => setInfoShown(true)} className={infoShown ? "info-icon-hidden" : "contact-icon"}></div>}
+            {pageEngaged && <div onClick={() => setInfoShown(false)}
+                onMouseEnter={() => setInfoShown(true)}
+                onTouchMove={() => setInfoShown(true)}
+                className={infoShown ? "contact-icon-hidden" : "contact-icon"}></div>}
             {pageEngaged && <div className={infoShown ? "contact-info-container" : "info-container-hidden"}>
                 <a href="mailto:athlarster@gmail.com" className="contact-item">Mail: athlarster@gmail.com</a>
                 <a href="https://linkedin.com" className="contact-item">Linkedin: fdfdfdf</a>
