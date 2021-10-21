a88b745346715eeca92e91451f2c4ebe360e22af
7Aylon <athlarster@gmail.com>
2021-10-21 10:13:06 +0200

    my project animation transition fixed on mobile

--- src/components/ProjectLink.jsx
+++ src/components/ProjectLink.jsx
@@ -3,13 +3,13 @@ import '../styles/my-projects.scss'
 
 function ProjectLink({ skills, description, gitLink, liveLink }) {
     return (
-        <div>
+        <>
             <div className={skills ? "projects-item-visible" : "projects-item"}>{description}</div>
             <div className={skills ? "link-container-visible" : "link-container"}>
                 <a className={skills ? "link-visible" : "link"} href={gitLink}>GitHub</a>
                 <a className={skills ? "link-visible-2" : "link-2"} href={liveLink}>Live</a>
             </div>
-        </div>
+        </>
     );
 }
 
