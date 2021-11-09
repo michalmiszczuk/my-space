1317cff406e365a98e39c5f91ea70a959c62d13d
7Aylon <athlarster@gmail.com>
2021-11-09 21:11:43 +0100

    typescript added to skills table

--- src/components/SkillsTable.jsx
+++ src/components/SkillsTable.jsx
@@ -7,7 +7,7 @@ function SkillsTable({ skillsTable }) {
                 <div id="skills-title">Skills acquired:</div>
                 <div className="skills-item" id="html">HTML</div>
                 <div className="skills-item" id="css">CSS (sass/scss)</div>
-                <div className="skills-item" id="javascript">Javascript</div>
+                <div className="skills-item" id="javascript">Javascript / Typescript</div>
                 <div className="skills-item" id="react">React</div>
                 <div className="skills-item" id="node">Node.js / Express</div>
                 <div className="skills-item" id="mongo">MongoDB</div>
